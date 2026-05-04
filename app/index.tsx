376f36849ef5f6c076dce173768985c76f9d7f65
Seydina Thiam <seydinat235@gmail.com>
2026-05-04 16:46:28 +0000

    chore: update dependencies and add new components
    - Updated package.json to include new dependencies:
      - @hookform/resolvers
      - @tanstack/react-query
      - axios
      - babel-preset-expo
      - date-fns
      - expo-camera
      - expo-device
      - expo-file-system
      - expo-image-manipulator
      - expo-image-picker
      - expo-notifications
      - expo-secure-store
      - nativewind
      - prettier-plugin-tailwindcss
      - react-hook-form
      - react-native-qrcode-svg
      - react-native-svg
      - socket.io-client
      - tailwindcss
      - zod
      - zustand
    
    - Added new components for events, feed, profile, shared UI elements, tickets, and hooks.
    - Created new configuration files for notifications, socket, and Tailwind CSS.
    - Added new API and type files for auth, events, feed, notifications, payments, tickets, and users.
    - Introduced new utility functions and validators for various schemas.

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-red-500 font-bold text-3xl">Eventflow</Text>
     </View>
   );
 }
